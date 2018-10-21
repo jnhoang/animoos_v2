24013d48ac725e079f0731f78553d9cdf0e199d7
alexmac3 <personal@Alexs-MacBook-Pro.local>
2018-10-21 10:50:31 -0700

    Adds Homepage and Navbar components

--- app/src/App.js
+++ app/src/App.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
+import Home from './Home';
+import Navbar from './Navbar';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
+	  <div>
+	  	  <Navbar />	
+      	  <Home />
       </div>
     );
   }
